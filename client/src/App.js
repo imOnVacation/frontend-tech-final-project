b8076e5fe5e0d141ba1127d95358c141006e3662
Leshi Chen <leshi@pdx.edu>
2024-11-13 19:19:07 -0800

    adding pages and modify the website layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Search from './pages/Search';
+import TicketCreation from './pages/TicketCreation';
+// import './style.css';
 
-function App() {
+export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg='dark' variant='dark' expand='lg'>
+        <Container>
+          <Navbar.Brand href='/'>Business Management Portal</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link as={Link} to='/'>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to='/dashboard'>
+                Dashboard
+              </Nav.Link>
+              <Nav.Link as={Link} to='/search'>
+                Search
+              </Nav.Link>
+              <Nav.Link as={Link} to='/ticketcreation'>
+                TicketCreation
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container className='my-4'>
+        <Routes>
+          <Route exact path='/' element={<Home title='Home Page' />} />
+          <Route
+            path='/dashboard'
+            element={<Dashboard title='Dashboard Page' />}
+          />
+          <Route path='/seasrch' element={<Search title='Search Page' />} />
+          <Route
+            path='/ticketcreation'
+            element={<TicketCreation title='Ticket Creation Page' />}
+          />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
-
-export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = ({ title }) => {
+  return (
+    <div>
+      <h1>This is the Home page</h1>
+      <h2>Welcome!</h2>
+    </div>
+  );
+};
+
+export default Home;
