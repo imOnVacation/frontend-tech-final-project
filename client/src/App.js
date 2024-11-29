1ca3b6697162afcebc60764382da7ee429505dc4
Leshi Chen <leshi@pdx.edu>
2024-11-28 16:47:09 -0800

    add route for displaying ticket by ticket status

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import Search from "./pages/Search";
-import TicketCreation from "./pages/TicketCreation";
-import TicketEdit from "./components/TicketEdit";
-import Header from "./components/Header";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Search from './pages/Search';
+import TicketCreation from './pages/TicketCreation';
+import TicketEdit from './components/TicketEdit';
+import Header from './components/Header';
+import TicketsByStatusAndMonth from './pages/TicketsByStatusAndMonth';
 
 export default function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/ticketcreation" element={<TicketCreation />} />
-        <Route path="/ticketedit/:ticketId" element={<TicketEdit />} />
+        <Route exact path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/ticketcreation' element={<TicketCreation />} />
+        <Route path='/ticketedit/:ticketId' element={<TicketEdit />} />
+        <Route path='/tickets/list' element={<TicketsByStatusAndMonth />} />
       </Routes>
     </Router>
   );
